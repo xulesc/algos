58ff1101c0e324f36924c7fa6ed7b78274f1e4a8
Anuj Sharma <anuj.sharma80@gmail.com>
2015-02-09 14:11:13 +0200

    refactor. correct for square matrix

--- ps_align/psalign.py
+++ ps_align/psalign.py
@@ -12,6 +12,14 @@ from Bio.QCPSuperimposer import QCPSuperimposer
 RESIDUE_LENGTH = 8
 RMS_THRESH = 0.00001
 
+## unequal array test case
+#d = np.array([[1, 2, 3, 6], [2, 4, 6, 5], [3, 6, 9, 1]]).astype(np.int32)
+#d.resize(4,4)
+#print d
+#c = np.array(munkers.run_munkers(d, 0)).reshape(d.shape)
+#print c[0:3, 0:4]
+#sys.exit(-1)
+
 def get_ca_atom_list(model):
   atoms = []; reses = []
   for chain in model:
@@ -24,9 +32,9 @@ def get_ca_atom_list(model):
   return (atoms, reses)
   
 def superimpose(p1, p2):
-    frms = 0; frot = []; ftran = []; p1_r = []; p2_r = []
+    frms = 0; frot = []; ftran = []; p1_r = []; p2_r = []; medges = 0
     mrms = sys.float_info.max
-    l1 = p1.shape[0]; l2 = p2.shape[0]
+    l1 = p1.shape[0]; l2 = p2.shape[0]; ml = max(l1, l2)
     for x in range(0, l1 - RESIDUE_LENGTH, RESIDUE_LENGTH):
         r1 = p1[x : x + RESIDUE_LENGTH]
         for y in range(0, l2 - RESIDUE_LENGTH, RESIDUE_LENGTH):
@@ -45,7 +53,9 @@ def superimpose(p1, p2):
             ## Note we are not rotating anything just finding a global non-sequential alignment
             ###################################################################################################################
             dist_matrix = distance.cdist(p1, dot(p2, rot) + tran, 'euclidean').astype(np.int32)
+            dist_matrix.resize((ml, ml))
             cost_matrix = np.array(munkers.run_munkers(dist_matrix, 0)).reshape(dist_matrix.shape)
+            cost_matrix = cost_matrix[0:l1, 0:l2]
             non_zero = cost_matrix > 0
             edges = np.column_stack(np.where(non_zero))
             a = np.array(map(lambda x : p1[x[0]], edges));  b = np.array(map(lambda x : p2[x[1]], edges));  
@@ -60,10 +70,11 @@ def superimpose(p1, p2):
                 ftran = tran
                 p1_r = [x, x + RESIDUE_LENGTH-1]
                 p2_r = [y, y + RESIDUE_LENGTH-1]
-    return [frms, frot, ftran, p1_r, p2_r]
+                medges = edges
+    return [frms, frot, ftran, p1_r, p2_r, edges]
             
 ## PDB domains
-pdomain1 = '1aa9.pdb'; pdomain2 = '6xia.pdb'
+pdomain1 = '1aa9.pdb'; pdomain2 = '1ash.pdb'
 ## parse protein structure files
 pdb_parser = Bio.PDB.PDBParser(QUIET = True)
 ref_structure = pdb_parser.get_structure("reference", "../pdb_data/%s" %pdomain1)
@@ -75,14 +86,10 @@ sample_structure = pdb_parser.get_structure("sample", "../pdb_data/%s" %pdomain2
 ref_coords = np.array(map(lambda x: x.get_coord(), ref_atoms))
 sample_coords = np.array(map(lambda x: x.get_coord(), sample_atoms))
 ##
-[rms, rot, tran, p1_r, p2_r] = superimpose(ref_coords, sample_coords)
-dist_matrix = distance.cdist(ref_coords, dot(sample_coords, rot) + tran, 'euclidean').astype(np.int32)
 t0 = datetime.now()
-cost_matrix = np.array(munkers.run_munkers(dist_matrix, 0)).reshape(dist_matrix.shape)
-non_zero = cost_matrix > 0
-edges = np.column_stack(np.where(non_zero))
-dif = datetime.now() - t0
-print "non-sequential threading: %d, %s, %d (msec)" %(len(edges), dist_matrix.shape, dif.total_seconds() * 1000)
+[rms, rot, tran, p1_r, p2_r, edges] = superimpose(ref_coords, sample_coords)
+dif = datetime.now()-t0
+print "non-sequential threading: %d, %s, %d (msec)" %(len(edges), [len(ref_atoms), len(sample_atoms)], dif.total_seconds() * 1000)
 print [('%s_%d' %(ref_reses[i].get_resname(), i), '%s-%d' %(sample_reses[j].get_resname(),j)) for i,j in edges]
 print "rmsd: %f" %rms
 print "rot: %s" %rot
