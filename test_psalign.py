8c23b9f1c199119897a976a09340ed13a2480044
Anuj Sharma <anuj.sharma80@gmail.com>
2015-03-15 07:21:40 +0200

    improve superimpose

--- test_psalign.py
+++ test_psalign.py
@@ -8,9 +8,13 @@ from numpy import sum, sqrt, dot
 from Bio.PDB.QCPSuperimposer import QCPSuperimposer
 from Bio.PDB.PSC.align import Align
 from scipy.spatial import distance
+import multiprocessing
 
 RESIDUE_LENGTH = 20
 RMS_THRESH = 0.001
+align = Align()
+aligner = [align.sequential, align.non_sequential]
+sup = QCPSuperimposer()
 
 def get_ca_atom_list(model):
   atoms = []; reses = []
@@ -23,49 +27,63 @@ def get_ca_atom_list(model):
         pass
   return (atoms, reses)
   
-def _superimpose(p1, p2, aligner, type):
-    sup = QCPSuperimposer(); align = Align()
-    lp2 = p2; previous_rms = sys.maxint; pedges = []
-    rms = -1; rot = []; tran = []; edges = []
-    while True:
-        edges = aligner(p1, lp2)
-        if type == 0:
-            s = set(edges) ^ set(pedges)
-            if len(pedges) != 0 and len(s) < 1.1 * len(pedges):
-                break
-            
-        if len(edges) == 1:
-            l = min(p1.shape[0], p2.shape[0])
-            edges = [(x, x) for x in range(l)]
-        pedges = edges
-        a = np.array(map(lambda x : p1[x[0]], edges));  b = np.array(map(lambda x : p2[x[1]], edges));
-        sup.set(a, b)
-        sup.run()
-        rms = sup.get_rms()
-        if type != 0:
-            if rms > previous_rms:
-                rms = previous_rms
-                break
-            else:
-                previous_rms = rms
-        rot, tran = sup.get_rotran()
-        lp2 = dot(p2, rot) + tran
-    return [rms, rot, tran, None, None, edges]
+def gentask(p1, p2, type):
+    i = 0
+    while i < p1.shape[0] - RESIDUE_LENGTH:
+        j = 0
+        while j < p2.shape[0] - RESIDUE_LENGTH:
+            yield [p1,p2,i,j,type]
+            j += RESIDUE_LENGTH 
+        i += RESIDUE_LENGTH
+
+def _task(task):
+    p1, p2, i, j, type = task
+    sup.set(p1[i:i+RESIDUE_LENGTH], p2[j:j+RESIDUE_LENGTH]); sup.run()  
+    u, t = sup.get_rotran()
+    lp2 = dot(p2, u) + t
+    edges = aligner[type](p1, lp2)
+    a = np.array(map(lambda x : p1[x[0]], edges))
+    b = np.array(map(lambda x : lp2[x[1]], edges))
+    sup.set(a, b); sup.run()
+    r = sup.get_rms()
+    return [i,j,(u,t),edges,r]
+  
+def _superimpose(p1, p2, type):
+    tasks = []; i = 0
+    while i < p1.shape[0] - RESIDUE_LENGTH:
+        j = 0
+        while j < p2.shape[0] - RESIDUE_LENGTH:
+            tasks.append([p1,p2,i,j,type])
+            j += RESIDUE_LENGTH
+        i += RESIDUE_LENGTH
+    for number_processes in range(1, 16):
+        pool = multiprocessing.Pool(number_processes)
+        t0 = datetime.now()
+        UsAndTs = pool.map(_task, tasks)
+        dif = datetime.now()-t0
+        if number_processes == 1:
+            told = dif.total_seconds() * 1000
+            tnew = told
+        else:
+            tnew = dif.total_seconds() * 1000
+        speedup = told * 1.0 / tnew
+        efficiency = speedup / number_processes
+        print '%d %f %f' %(number_processes,speedup,efficiency)
+        
+    ###
+    ret = np.array(map(lambda x: x[4], UsAndTs))
+    i, j, (rot, tran), pedges, rms = UsAndTs[np.argmin(ret)]
+    return [rms, rot, tran, None, None, pedges]
     
 def superimpose(p1, p2, type = 0):
-    align = Align()
-    if type == 0:
-        aligner = align.sequential
-    else:
-        aligner = align.non_sequential
     t0 = datetime.now()
-    [rms, rot, tran, p1_r, p2_r, edges] = _superimpose(ref_coords, sample_coords, aligner, type)
+    [rms, rot, tran, p1_r, p2_r, edges] = _superimpose(ref_coords, sample_coords, type)
     dif = datetime.now()-t0
-    print "%s: %d, %s, %d (msec)" %(type, len(edges), [len(ref_atoms), len(sample_atoms)], dif.total_seconds() * 1000)
-    print [('%s_%d' %(ref_reses[i].get_resname(), i), '%s-%d' %(sample_reses[j].get_resname(),j)) for i,j in edges]
-    print "rmsd: %f" %rms
-    print "rot: %s" %rot
-    print "tran: %s" %tran
+    print "#%s: %d, %s, %d (msec)" %(type, len(edges), [len(ref_atoms), len(sample_atoms)], dif.total_seconds() * 1000)
+    print "#%s" %([('%s_%d' %(ref_reses[i].get_resname(), i), '%s-%d' %(sample_reses[j].get_resname(),j)) for i,j in edges])
+    print "#rmsd: %f" %rms
+    print "#rot: %s" %(','.join(map(lambda x: '%f %f %f' %(x[0],x[1],x[2]), rot)))
+    print "#tran: %s" %tran
     
 def ssuperimpose(p1, p2):
     superimpose(p1, p2)
@@ -74,7 +92,12 @@ def nssuperimpose(p1, p2):
     superimpose(p1, p2, 1)
 
 ## PDB domains
-pdomain1 = '1aa9.pdb'; pdomain2 = '6xia.pdb'
+if len(sys.argv) < 3:
+   print 'usage: python test_psalign.py dom1 dom2 '
+   sys.exit(-1)
+pdomain1 = '%s.pdb' %sys.argv[1]
+pdomain2 = '%s.pdb' %sys.argv[2]
+print '#%s %s' %(pdomain1,pdomain2)
 ## parse protein structure files
 pdb_parser = Bio.PDB.PDBParser(QUIET = True)
 ref_structure = pdb_parser.get_structure("reference", "pdb_test_data/%s" %pdomain1)
@@ -88,4 +111,4 @@ sample_coords = np.array(map(lambda x: x.get_coord(), sample_atoms))
 ## sequential
 ssuperimpose(ref_coords, sample_coords)
 ## non-sequential
-nssuperimpose(ref_coords, sample_coords)                                                                                                    
+#nssuperimpose(ref_coords, sample_coords)                                                                                                    
