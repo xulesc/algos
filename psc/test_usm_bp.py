448d4095c409a39fe110af7319d89db0c65da24f
Anuj Sharma <anuj.sharma80@gmail.com>
2015-03-31 15:46:23 +0300

    test case for biopython usm

---
+++ psc/test_usm_bp.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import Bio.PDB.PDBParser
+from Bio.PDB.PSC.usm import USM
+import os
+import numpy as np
+
+def get_ca_atom_list(model):
+  atoms = []
+  for chain in model:
+    for res in chain:
+      try:
+        atoms.append(res['CA'])
+      except:
+        pass
+  return atoms
+   
+def get_contact_map_complexities(in_dir):
+  usm = USM()
+  pdb_parser = Bio.PDB.PDBParser(QUIET = True)
+  structure_cm_string = {}; 
+
+  for filename in os.listdir(in_dir):
+    structure = pdb_parser.get_structure("reference", "%s/%s" %(in_dir, filename))
+    coords = np.array(map(lambda x : x.coord, get_ca_atom_list(structure[0]))) 
+    structure_cm_string[filename] = usm.get_contact_map(coords)[1]
+    
+  for k1, v1 in structure_cm_string.iteritems():
+    for k2, v2 in structure_cm_string.iteritems():
+      dist = usm.dist(structure_cm_string[k1], structure_cm_string[k2])
+      print '%s - %s : %f' %(k1, k2, dist)  
+
+if __name__ == '__main__':  
+  np.set_printoptions(threshold='nan')
+  get_contact_map_complexities('pdb_data')
