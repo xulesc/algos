8072d09a4d1cb805fe16240200751ba1f6390f70
Anuj Sharma <anuj.sharma80@gmail.com>
2015-08-18 07:59:58 +0300

    basic implementation

--- subset.py
+++ subset.py
@@ -5,6 +5,7 @@
 #
 
 import numpy as np
+import random
 
 class SubsetGenerator:
     def __init__(self, fname=None):
@@ -14,7 +15,7 @@ class SubsetGenerator:
         self._SEP2 = ' '
         self._skipf = lambda x : not x.startswith('#')
         if fname != None:
-            self.data = self.__readsparsedata()
+            self.data = self.__read_sparse_data()
 
     def __read_line(self, l):
         zipped = map(lambda x : tuple(x.split(self._SEP1)), l.split(self._SEP2))
@@ -26,7 +27,7 @@ class SubsetGenerator:
 
     def __read_sparse_data(self):
         # read data
-        vectors = map(self.__readline, filter(self._skipf, open(self._fname)))
+        vectors = map(self.__read_line, filter(self._skipf, open(self._fname)))
         # find longest vector
         max_entries = max(map(len, vectors))
         # create masked vectors
@@ -34,12 +35,29 @@ class SubsetGenerator:
             mask=np.concatenate([np.zeros(len(b),dtype=bool),
                                  np.ones(max_entries-len(b), dtype=bool)]))
         # return square np matrix
-        return np.array(map(maskf, data))
+        return np.array(map(maskf, vectors))
 
     def get_data(self): return self.data
-        
+
+    def make_subset(self, similarity=0.25):        
+        # make L2-norm per row
+        norms = map(np.linalg.norm, self.data)
+        # convert norms to probabilities
+        norms /= sum(norms)
+        # make CDF
+        cdf = np.cumsum(norms)
+        # make subset
+        subset = []; visited = {}
+        while True:
+            idx = np.searchsorted(cdf, random.uniform(0, 1), sorter=None)
+            if idx in visited.keys(): continue
+            visited[idx] = 1
+            subset.append(idx)
+            if sum(norms[visited.keys()]) >= similarity: break
+        return self.data[subset]
         
 if __name__ == '__main__':
     test_file_name = 'subset.dat'
     s = SubsetGenerator(test_file_name)
     print s.get_data()
+    print s.make_subset()
