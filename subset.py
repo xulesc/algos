46e7e09a2046805f3ffec0c2fdb37d8ce2d86582
Anuj Sharma <anuj.sharma80@gmail.com>
2015-08-18 05:43:40 +0300

    add subset generator

---
+++ subset.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+class SubsetGenerator:
+    def __init__(self, fname=None):
+        self.fname = fname
+
+    def __readline(self, line):
+        dim_data = split(' ')
+        max_dim = max(map(lambda x : x.split(':')[0], dim_data))
+
+    def __readsparsedata__(self):
+        
